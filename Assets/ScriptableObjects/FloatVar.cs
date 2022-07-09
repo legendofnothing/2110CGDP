679f6015c9c96510eb4fc7a52da9c1e6da664a10
legendofnothing <huyrtb123@gmail.com>
2022-07-10 01:37:20 +0700

    Setup Project

---
+++ Assets/ScriptableObjects/FloatVar.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "FloatVar", menuName = "ScriptableObjects/Float Vars", order = 1)]
+
+public class FloatVar : ScriptableObject {
+    public float Value;
+}
---
+++ Assets/ScriptableObjects/IntVar.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "IntVar", menuName = "ScriptableObjects/Int Vars", order = 2)]
+
+public class IntVar : ScriptableObject {
+    public float Value;
+}
