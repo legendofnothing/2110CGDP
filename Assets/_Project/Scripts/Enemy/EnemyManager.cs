a99d99749d0d35b05fdf812c4f1ae9e8060795ce
legendofnothing <huyrtb123@gmail.com>
2022-07-11 22:34:30 +0700

    Add EnemyManager, Add TMPro

---
+++ Assets/_Project/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Summary: Handles Enemy States, HP etc
+//Created by: leg
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("Enemy Config")]
+    public float enemyHP;
+
+    [Header("Other Config")]
+    public TextMesh enemyHPDisplay;
+
+    private float _currentHP;
+
+    private void Start() {
+        _currentHP = enemyHP;
+    }
+ 
+    private void Update() {
+        enemyHPDisplay.text = "HP:" + _currentHP.ToString("0");
+    }
+
+    public void DealDamage(float dmg) {
+        _currentHP -= dmg;
+    }
+
+}
