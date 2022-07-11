ec3d0c441a4486a4ca43617368dbe54cc757170f
legendofnothing <huyrtb123@gmail.com>
2022-07-11 20:57:24 +0700

    PlayerController: Add Jumping

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -7,12 +7,21 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerSpeed;
+    [Header("Player Configs")]
+    public float playerSpeed;   
+    public float playerJumpPower;
+
+    [Header("Other Configs")]
+    public LayerMask groundLayer;
 
     Rigidbody2D rb;
 
     private void Start() {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        Jump();
     }
 
     private void FixedUpdate() {
@@ -22,10 +31,26 @@ public class PlayerController : MonoBehaviour
     //Handles Movement
     private void Movement() {
         //Get horiztonal Input
-        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var horizontalInput = Input.GetAxis("Horizontal");
 
         //Move by using rigidBody2D.velocity
         rb.velocity = new Vector2(horizontalInput * playerSpeed, rb.velocity.y);
     }
 
+    //Handles Jumping 
+    private void Jump() {
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+
+            rb.AddForce(transform.up * playerJumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    //Return true if player is hitting ground, else return false
+    private bool IsGrounded() {
+        //Cast a ray straight down
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+
+        if (hit.collider == null) return false; else return true;
+    }
 }
