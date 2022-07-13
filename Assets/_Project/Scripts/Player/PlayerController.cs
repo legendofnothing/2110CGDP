e0320689f9300f314e280d7cf6a6d3eb72dd52f8
legendofnothing <huyrtb123@gmail.com>
2022-07-14 00:42:39 +0700

    Adjust Dodging, Add Player prefab

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     [Header("Other Configs")]
     public LayerMask groundLayer;
 
+    private bool _isMoving;
+    private bool _isDodging;
+
+    private bool _canDodge = true;
+
     Rigidbody2D rb;
 
     private void Start() {
@@ -22,13 +27,13 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update() {
-        Jump();
+        if (!_isDodging) Jump();
 
-        Dodge();
+        if (_isMoving) Dodge();
     }
 
     private void FixedUpdate() {
-        Movement();
+        if (!_isDodging) Movement();
     }
 
     //Handles Movement
@@ -45,6 +50,9 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput > 0) transform.eulerAngles = new Vector3(0, 0, 0); //Right
 
         else transform.eulerAngles = gameObject.transform.eulerAngles;
+
+        //Handle isMoving Condition
+        if (horizontalInput > 0 || horizontalInput < 0) _isMoving = true; else _isMoving = false;
     }
 
     //Handles Jumping 
@@ -56,6 +64,17 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Handles dodging 
+    private void Dodge()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _canDodge)
+        {
+            var horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            if (horizontalInput > 0) StartCoroutine(Dodging(1)); else StartCoroutine(Dodging(-1));
+        }
+    }
+
     //Return true if player is hitting ground, else return false
     private bool IsGrounded() {
         //Cast a ray straight down
@@ -64,12 +83,29 @@ public class PlayerController : MonoBehaviour
         if (hit.collider == null) return false; else return true;
     }
 
-    //Handles dodging 
-    private void Dodge()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            rb.AddRelativeForce(transform.right * playerDodgePower);
-        }
+
+    //Coroutine for dodging
+    private IEnumerator Dodging(float dir) {
+        //Store old Gravity Scale Value
+        var oldGravityScale = rb.gravityScale;
+
+        _isDodging = true;
+        _canDodge = false;
+
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+
+        rb.AddForce(new Vector2(playerDodgePower * dir, 0f), ForceMode2D.Impulse); //Dogding Part
+
+        yield return new WaitForSeconds(0.4f); //Dodging Duration (can turn this into a variable)
+
+        rb.gravityScale = oldGravityScale;
+        rb.velocity = Vector2.zero;
+
+        _isDodging = false;
+
+        yield return new WaitForSeconds(0.4f); //Dodging Cooldown (can turn this into a variable)
+
+        _canDodge = true;
     }
 }
