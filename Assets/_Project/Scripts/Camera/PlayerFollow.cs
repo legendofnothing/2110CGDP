5387df127796a72e3682e8f31341a37346a48be8
operatorprime21 <105938958+operatorprime21@users.noreply.github.com>
2022-07-13 23:14:14 +0700

    Cam Controls
    all changes made in prime1 scene

---
+++ Assets/_Project/Scripts/Camera/PlayerFollow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform playerPos;
+    private bool _outOfCam;
+    private Vector3 _playerCamPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerCamPos.z = this.transform.position.z;
+        if (_outOfCam)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, _playerCamPos, 0.01f);
+        }
+        //cam.transform.position = Vector3.MoveTowards(cam.transform.position, playerPos.position, 1);
+        if(playerPos.position.x < this.transform.position.x - 3 || playerPos.position.x > this.transform.position.x + 3)
+        {
+            _playerCamPos.y = this.transform.position.y;
+            _playerCamPos.x = playerPos.position.x;
+            _outOfCam = true;
+        }
+        else if (playerPos.position.y > this.transform.position.y + 1.5f || playerPos.position.y < this.transform.position.y - 1.5f)
+        {
+            _playerCamPos.x = this.transform.position.x;
+            _playerCamPos.y = playerPos.position.y;
+            _outOfCam = true;
+        }
+        else 
+        {
+            _outOfCam = false;
+        }
+    }
+}
---
+++ Assets/_Project/Sprites/TestAssets/Notes.cs
@@ -0,0 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TEST ASSETS. NOT FINAL DO NOT USE//
