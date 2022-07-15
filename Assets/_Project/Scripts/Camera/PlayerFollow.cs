90f0fe621b68d7e8b2ee52738d38b0ecf4088f50
operatorprime21 <105938958+operatorprime21@users.noreply.github.com>
2022-07-15 18:08:44 +0700

    Flip added. Replaced normal cam with Cinemachine
    Flip is on a separate script, integrate into movement if can. If anyone used the previous PlayerFollow script for the camera, remove it.

--- Assets/_Project/Scripts/Camera/PlayerFollow.cs
+++ Assets/_Project/Scripts/Camera/PlayerFollow.cs
@@ -1,43 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerFollow : MonoBehaviour
 {
-    public Transform playerPos;
-    private bool _outOfCam;
-    private Vector3 _playerCamPos;
-    public float camSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //private float _playerScale;
+    //public CinemachineVirtualCamera thisCam;
+    //private float _shiftTime;
+    //private float _screenX;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.5f;
+    //    _screenX = 0.5f;
+    //}
 
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    _playerScale = GameObject.Find("Player").GetComponent<Transform>().rotation.y;
+    //    thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = _screenX;
 
-    // Update is called once per frame
-    void Update()
-    {
-        _playerCamPos.z = this.transform.position.z;
-        if (_outOfCam)
-        {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, _playerCamPos, camSpeed);
-        }
-        //cam.transform.position = Vector3.MoveTowards(cam.transform.position, playerPos.position, 1);
-        if(playerPos.position.x < this.transform.position.x - 3 || playerPos.position.x > this.transform.position.x + 3)
-        {
-            _playerCamPos.y = this.transform.position.y;
-            _playerCamPos.x = playerPos.position.x;
-            _outOfCam = true;
-        }
-        else if (playerPos.position.y > this.transform.position.y + 1.5f || playerPos.position.y < this.transform.position.y - 1.5f)
-        {
-            _playerCamPos.x = this.transform.position.x;
-            _playerCamPos.y = playerPos.position.y;
-            _outOfCam = true;
-        }
-        else 
-        {
-            _outOfCam = false;
-        }
-    }
+    //    if (_playerScale == 0f)
+    //    {
+    //        thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.4f;
+    //    }
+    //    else if (_playerScale == -1f)
+    //    {
+    //        thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.6f;
+    //    }
+    //}
+
+    //IEnumerator ShiftCam(float dur, float newScreenX)
+    //{
+    //    //float elapsed = 0.0f;
+    //    //while (elapsed < dur)
+    //    //{
+    //    //    _screenX = Mathf.Lerp(_screenX, newScreenX, elapsed / dur);
+    //    //    elapsed += Time.deltaTime;
+    //    //    yield return null;
+    //    //}
+    //    //_screenX = newScreenX;
+    //    yield return new WaitForSeconds(dur);
+    //    _screenX = newScreenX;
+    //}
 }
