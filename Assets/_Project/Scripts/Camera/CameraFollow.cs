29c004a8d72ce81613d30cc0fad32f866955404a
operatorprime21 <105938958+operatorprime21@users.noreply.github.com>
2022-07-28 00:00:43 +0700

    Script comment

--- Assets/_Project/Scripts/Camera/CameraFollow.cs
+++ Assets/_Project/Scripts/Camera/CameraFollow.cs
@@ -5,44 +5,5 @@ using Cinemachine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //private float _playerScale;
-    //public CinemachineVirtualCamera thisCam;
-    //private float _shiftTime;
-    //private float _screenX;
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.5f;
-    //    _screenX = 0.5f;
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    _playerScale = GameObject.Find("Player").GetComponent<Transform>().rotation.y;
-    //    thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = _screenX;
-
-    //    if (_playerScale == 0f)
-    //    {
-    //        thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.4f;
-    //    }
-    //    else if (_playerScale == -1f)
-    //    {
-    //        thisCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = 0.6f;
-    //    }
-    //}
-
-    //IEnumerator ShiftCam(float dur, float newScreenX)
-    //{
-    //    //float elapsed = 0.0f;
-    //    //while (elapsed < dur)
-    //    //{
-    //    //    _screenX = Mathf.Lerp(_screenX, newScreenX, elapsed / dur);
-    //    //    elapsed += Time.deltaTime;
-    //    //    yield return null;
-    //    //}
-    //    //_screenX = newScreenX;
-    //    yield return new WaitForSeconds(dur);
-    //    _screenX = newScreenX;
-    //}
+    //this cam has footage of me having reproductive intercourse with your maternal figure
 }
